5063a9ef0ca963b4334df1750260f6cc84e6d54b
BerkantKursuz <berkantkursuz@gmail.com>
2021-07-06 00:19:24 +0300

    Ufak tefek değişikler
    Morgan ekledim siteyi parçaladık biraz sağı solu

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 app.set('view engine', 'ejs')
-
+app.use(express.static('public'))
 app.listen(3000)
 
 //middleware
 
+app.use(morgan('tiny'))
 
 //Routes
 app.get('/',(req,res)=>{
